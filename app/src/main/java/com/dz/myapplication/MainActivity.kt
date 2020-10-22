345ab1b571ce41462f0cd0224e79bb01772d5008
HL88888888 <365759230@qq.com>
2020-10-22 11:02:15 +0800

    test_01_01

--- app/src/main/java/com/dz/myapplication/MainActivity.kt
+++ app/src/main/java/com/dz/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var version="test_01_00"
+        var version="test_01_01"
     }
 }
